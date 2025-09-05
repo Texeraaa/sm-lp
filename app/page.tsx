bb86cdb742878a4f07a5005e81ef1945002cc9af
Texeraaa <pedro.henrique.ph2018@hotmail.com>
2025-09-05 09:28:09 -0300

    feat: service page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 export default function Home() {
   return (
-   <Hero />
+    <div className="min-h-screen">
+      <Hero />
+      <Services />
+    </div>
   );
 }
